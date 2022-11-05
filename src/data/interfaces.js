54514dcc7c8b8309a24d67bf539e7da8685e9b1d
Octo <octo@clubpenguin.plus>
2022-11-05 21:13:37 +0000

    oop

--- src/data/interfaces.js
+++ src/data/interfaces.js
@@ -8,7 +8,6 @@ const interfaces = {
     Hydro: require('@scenes/interface/catalogs/hydro/Hydro').default,
     Waves: require('@scenes/interface/catalogs/waves/Waves').default,
     Fishing: require('@scenes/interface/catalogs/fishing/Fishing').default,
-    Stampbook: require('@scenes/interface/game/stampbook/Stampbook').default,
     SportsCatalog: require('@scenes/interface/catalogs/sports/SportsCatalog').default,
     CostumeCatalog: require('@scenes/interface/catalogs/costume/CostumeCatalog').default,
     ModPanel: require('@scenes/interface/menus/mod/ModPanel').default,
