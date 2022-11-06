9e68d14394688b7562d89d989a776b1e3effe5b7
Octo <octo@clubpenguin.plus>
2022-11-06 16:20:08 +0000

    fix failed loading

--- src/data/fonts.js
+++ src/data/fonts.js
@@ -6,7 +6,7 @@ const fonts = [
     {name: 'Asterisk', url: 'https://media.cpplus.pw/client/fonts/Asterisk.woff2'},
     {name: 'Burbank Small', url: 'https://media.cpplus.pw/client/fonts/BurbankSmallMedium.woff2'},
     {name: 'Burbank Small', weight: 'bold', url: 'https://media.cpplus.pw/client/fonts/BurbankSmallBold.woff2'},
-    {name: 'CCComiccrazy', url: 'https://media.cpplus.pw/client/fonts/CCComiccrazy-Roman.woff2'},
+    {name: 'CCComiccrazy', url: 'https://media.cpplus.pw/client/fonts/CCComiccrazyRoman.woff2'},
     {name: 'CCFaceFront', weight: 'bold', style: 'italic', url: 'https://media.cpplus.pw/client/fonts/CCFaceFrontBoldItalic.woff2'},
 ]
 
