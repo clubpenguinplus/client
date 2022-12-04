f1991735eabb23e487564814a8cd83ffcbe7aade
Octo <octo@clubpenguin.plus>
2022-12-04 14:51:14 +0000

    change boxdim to use webm [skip ci]

--- src/scenes/rooms/boxdimension/BoxDimension.js
+++ src/scenes/rooms/boxdimension/BoxDimension.js
@@ -8,6 +8,12 @@ export default class BoxDimension extends RoomScene {
     constructor() {
         super('BoxDimension')
 
+        /** @type {Phaser.GameObjects.Container} */
+        this.leftboxes
+        /** @type {Phaser.GameObjects.Container} */
+        this.portal
+        /** @type {Phaser.GameObjects.Container} */
+        this.frontboxes
         /** @type {Array<any>} */
         this.sort
 
@@ -32,11 +38,11 @@ export default class BoxDimension extends RoomScene {
         // bg
         this.add.image(760, 480, 'boxdimension', 'bg')
 
-        // leftboxes0001
-        const leftboxes0001 = this.add.sprite(485, 327, 'boxdimension', 'leftboxes0001')
+        // leftboxes
+        const leftboxes = this.add.container(485, 327)
 
-        // portal0001
-        const portal0001 = this.add.sprite(1232, 159, 'boxdimension', 'portal0001')
+        // portal
+        const portal = this.add.container(1232, 159)
 
         // aboveportal
         this.add.image(1193, 237, 'boxdimension', 'aboveportal')
@@ -47,38 +53,42 @@ export default class BoxDimension extends RoomScene {
         // bg_1
         this.add.image(765, 585.5, 'boxdimension', 'boxes')
 
-        // frontboxes0001
-        const frontboxes0001 = this.add.sprite(753, 811, 'boxdimension', 'frontboxes0001')
+        // frontboxes
+        const frontboxes = this.add.container(753, 811)
 
         // lists
         const sort = []
-
-        // leftboxes0001 (components)
-        const leftboxes0001Animation = new Animation(leftboxes0001)
-        leftboxes0001Animation.key = 'leftboxes'
-        leftboxes0001Animation.end = 110
-
-        // portal0001 (components)
-        const portal0001Animation = new Animation(portal0001)
-        portal0001Animation.key = 'portal'
-        portal0001Animation.end = 100
 
         // rightbox0001 (components)
         const rightbox0001Animation = new Animation(rightbox0001)
         rightbox0001Animation.key = 'rightbox'
         rightbox0001Animation.end = 110
 
-        // frontboxes0001 (components)
-        const frontboxes0001Animation = new Animation(frontboxes0001)
-        frontboxes0001Animation.key = 'frontboxes'
-        frontboxes0001Animation.end = 110
-
+        this.leftboxes = leftboxes
+        this.portal = portal
+        this.frontboxes = frontboxes
         this.sort = sort
 
         this.events.emit('scene-awake')
     }
 
     /* START-USER-CODE */
+
+    create() {
+        super.create()
+
+        let frontboxes = this.add.video(0, 0, 'boxdimension-frontboxes')
+        this.frontboxes.add(frontboxes)
+        frontboxes.play(true)
+
+        let leftboxes = this.add.video(0, 0, 'boxdimension-leftboxes')
+        this.leftboxes.add(leftboxes)
+        leftboxes.play(true)
+
+        let portal = this.add.video(0, 0, 'boxdimension-portal')
+        this.portal.add(portal)
+        portal.play(true)
+    }
 
     /* END-USER-CODE */
 }
