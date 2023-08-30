764f07f7cbe29ac6b2c24746eae020b8cfd11c90
Octo <octo@clubpenguin.plus>
2023-08-30 23:26:37 +0100

    greatly improve speed of client commit by only minifying modified jsons

--- utils/minify-json.js
+++ utils/minify-json.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const {exitCode} = require('process')
+const exec = require('child_process').exec
 
 function searchFolder(path) {
     let files = fs.readdirSync(path)
@@ -9,15 +11,44 @@ function searchFolder(path) {
         }
 
         if (file.endsWith('.json')) {
-            var jsonFile = fs.readFileSync(`${path}/${file}`, 'utf8')
-            var jsonData = JSON.parse(jsonFile)
-            if (path.includes('crumbs/')) {
-                fs.writeFileSync(`${path}/${file}`, JSON.stringify(jsonData, null, 4))
-            } else {
-                fs.writeFileSync(`${path}/${file}`, JSON.stringify(jsonData))
-            }
+            minify(`${path}/${file}`)
         }
     }
 }
 
-searchFolder('./client/media')
+function minify(json) {
+    var jsonFile = fs.readFileSync(json, 'utf8')
+    var jsonData = JSON.parse(jsonFile)
+    if (json.includes('crumbs/')) {
+        fs.writeFileSync(json, JSON.stringify(jsonData, null, 4))
+    } else {
+        fs.writeFileSync(json, JSON.stringify(jsonData))
+    }
+}
+
+if (process.argv.join(' ').includes('--search')) {
+    searchFolder('./client/media')
+    return
+}
+
+exec(
+    'git status',
+    {
+        cwd: './client'
+    },
+    (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        if (stderr) {
+            console.error(stderr)
+        }
+        for (let line of stdout.split('\n')) {
+            if (line.endsWith('.json')) {
+                let file = line.split(' ')[line.split(' ').length - 1]
+                minify(`client/${file}`)
+            }
+        }
+    }
+)
