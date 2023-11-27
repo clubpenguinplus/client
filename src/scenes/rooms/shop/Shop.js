e339969d5a07646bfa99cb09661fd1d689d47a5e
JorkDev <jorkaefdev@gmail.com>
2023-11-27 12:20:44 -0500

    skylar

--- src/scenes/rooms/shop/Shop.js
+++ src/scenes/rooms/shop/Shop.js
@@ -9,44 +9,18 @@ export default class Shop extends RoomScene {
         super('Shop')
 
         /** @type {Phaser.GameObjects.Sprite} */
-        this.bg
+        this.cash
         /** @type {Phaser.GameObjects.Sprite} */
-        this.rail
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.speakers
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.chairBody
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.chairArm
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.chairBody_1
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.chairArm_1
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.deskChair
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.desk
+        this.train
         /** @type {Phaser.GameObjects.Sprite} */
         this.chest
         /** @type {Phaser.GameObjects.Sprite} */
-        this.register
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.clothesroom
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.spinner
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.door
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.circularStool
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.squareStool
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.fg
-        /** @type {Phaser.GameObjects.Sprite} */
-        this.catalog_en
+        this.catalog_old
         /** @type {Phaser.GameObjects.Sprite} */
         this.unlock
-        /** @type {Array<Phaser.GameObjects.Sprite|Phaser.GameObjects.Container>} */
+        /** @type {Phaser.GameObjects.Sprite} */
+        this.catalog
+        /** @type {Array<Phaser.GameObjects.Sprite|Phaser.GameObjects.Image>} */
         this.sort
 
         /* START-USER-CTR-CODE */
@@ -55,7 +29,7 @@ export default class Shop extends RoomScene {
             town: () => this.triggerRoom(100, 932, 560)
         }
         this.music = 345
-        this.loadSfx = ['shop-curtainopen', 'shop-curtainclose', 'shop-spinner', 'shop-dooropen', 'shop-doorclose', 'shop-bookopen', 'shop-bookclose']
+        this.loadSfx = ['coffee-cashregister', 'shop-curtainopen', 'shop-curtainclose', 'shop-spinner', 'shop-dooropen', 'shop-doorclose', 'shop-bookopen', 'shop-bookclose']
 
         /* END-USER-CTR-CODE */
     }
@@ -67,129 +41,145 @@ export default class Shop extends RoomScene {
 
     /** @returns {void} */
     _create() {
+        // shopdoor
+        const shopdoor = this.add.image(1112, 255, 'shop', 'shopdoor')
+
         // bg
-        const bg = this.add.sprite(765, 468, 'shop', 'bg')
+        this.add.image(760, 452, 'shop', 'bg')
 
-        // rail
-        const rail = this.add.sprite(363, 472, 'shop', 'rail')
+        // dinosaur
+        this.add.image(1501, 392, 'shop', 'dinosaur')
 
-        // speakers
-        const speakers = this.add.sprite(762, 224, 'shop', 'speakers0001')
+        // container_1
+        const container_1 = this.add.container(1332.7455296300423, 430.4237691679065)
 
-        // chairBody
-        const chairBody = this.add.sprite(282, 640, 'shop', 'chairBody')
+        // table
+        const table = this.add.image(20.88157135767574, -3.8984094567359193, 'shop', 'table')
+        table.setOrigin(0.44011293667673024, 0.09712339218891278)
+        container_1.add(table)
 
-        // chairArm
-        const chairArm = this.add.sprite(301, 686, 'shop', 'chairArm')
+        // cash
+        const cash = this.add.sprite(49.37309729222375, 1.694866337428266, 'shop', 'cashJump0001')
+        cash.setOrigin(0.4085058754783383, 0.4203115689650038)
+        container_1.add(cash)
 
-        // chairBody_1
-        const chairBody_1 = this.add.sprite(336, 767, 'shop', 'chairBody')
+        // cabinet
+        this.add.image(258, 404, 'shop', 'cabinet')
 
-        // chairArm_1
-        const chairArm_1 = this.add.sprite(355, 814, 'shop', 'chairArm')
+        // pillar
+        this.add.sprite(989, 251, 'pillar')
 
-        // deskChair
-        const deskChair = this.add.sprite(1370, 380, 'shop', 'chair')
+        // train
+        const train = this.add.sprite(572, 121, 'shop', 'train0001')
 
-        // desk_container
-        const desk_container = this.add.container(1337.3141623444612, 415.3218257529371)
-
-        // desk
-        const desk = this.add.sprite(13.685837655538762, 45.678174247062884, 'shop', 'desk')
-        desk_container.add(desk)
+        // carts
+        this.add.image(749, 243, 'shop', 'carts')
 
         // chest
-        const chest = this.add.sprite(15.685837655538762, 123.67817424706288, 'shop', 'chest')
-        desk_container.add(chest)
+        const chest = this.add.sprite(566, 329, 'shop', 'chest')
 
-        // register
-        const register = this.add.sprite(-48.31416234446124, -46.321825752937116, 'shop', 'register0001')
-        desk_container.add(register)
+        // bears
+        const bears = this.add.image(609, 468, 'shop', 'bears')
 
-        // clothesroom
-        const clothesroom = this.add.sprite(459, 284, 'shop', 'clothesroom')
+        // bucket_1
+        this.add.image(1399, 563, 'shop', 'bucket_1')
 
-        // spinner
-        const spinner = this.add.sprite(1403, 436, 'shop', 'spinner0001')
+        // bucket_2
+        this.add.image(1477, 594, 'shop', 'bucket_2')
 
-        // door
-        const door = this.add.sprite(1103, 248, 'shop', 'door')
+        // plushes
+        const plushes = this.add.image(703, 852, 'shop', 'plushes')
 
-        // circularStool
-        const circularStool = this.add.sprite(1105.0536438593308, 585.1647632822302, 'shop', 'circularStool')
-        circularStool.setOrigin(0.3927203373794677, 0.1307307179502602)
+        // puffles
+        const puffles = this.add.image(565, 599, 'shop', 'puffles')
 
-        // squareStool
-        const squareStool = this.add.sprite(1032.9632984323307, 645.1017420341761, 'shop', 'squareStool')
-        squareStool.setOrigin(0.3475601335676138, 0.1008847133406621)
+        // pucks
+        this.add.image(1243, 833, 'shop', 'pucks')
 
-        // fg
-        const fg = this.add.sprite(782, 980.5, 'shop', 'fg')
-        fg.setOrigin(0.5, 1)
+        // race_3
+        const race_3 = this.add.image(1077.2456340817034, 489.42103277154115, 'shop', 'race_3')
+        race_3.setOrigin(0.9431532210870683, 0.9325425101596304)
 
-        // catalog-en
-        const catalog_en = this.add.sprite(1340, 1055, 'shop', 'catalog-en')
-        catalog_en.setOrigin(0, 2)
+        // race_1
+        this.add.image(1083, 662, 'shop', 'race_1')
+
+        // race_2
+        const race_2 = this.add.image(857.787292618592, 427.0395074111094, 'shop', 'race_2')
+        race_2.setOrigin(0.20254797775389477, 0.10987654698508967)
+
+        // trafficlights
+        const trafficlights = this.add.image(860.8574576920828, 438.9671098800855, 'shop', 'traffic0001')
+        trafficlights.setOrigin(0.6369607718841335, 0.39941970014847006)
+
+        // toy
+        const toy = this.add.image(1106, 796, 'shop', 'toy')
+
+        // toy_1
+        const toy_1 = this.add.image(915, 815, 'shop', 'toy')
+
+        // tribune_front
+        this.add.image(220, 743, 'shop', 'tribune_front')
+
+        // tribune_back
+        const tribune_back = this.add.image(123, 705, 'shop', 'tribune_back')
+
+        // small_overlay
+        const small_overlay = this.add.image(624, 114, 'shop', 'small_overlay')
+
+        // overlay
+        const overlay = this.add.image(752, 490, 'shop', 'overlay')
+
+        // catalog_old
+        const catalog_old = this.add.sprite(1644, 1057, 'shop', 'catalog')
+        catalog_old.setOrigin(0, 2)
 
         // unlock
         const unlock = this.add.sprite(1415, 756, 'shop', 'unlock')
         unlock.visible = false
 
+        // catalog
+        const catalog = this.add.sprite(1335, 1087, 'shop', 'catalog')
+        catalog.setOrigin(-0.02426799776994842, 2.2833918797658597)
+
         // lists
-        const sort = [catalog_en, fg, squareStool, circularStool, door, spinner, deskChair, chairArm_1, chairBody_1, chairArm, chairBody, speakers, rail, desk_container]
+        const sort = [catalog_old, plushes, puffles, bears, toy_1, toy, tribune_back, race_2, trafficlights, race_3, overlay, small_overlay, table, cash, catalog]
 
-        // clothesroom (components)
-        const clothesroomButton = new Button(clothesroom)
-        clothesroomButton.hoverCallback = () => this.shell.musicController.addSfx('shop-curtainopen')
-        clothesroomButton.hoverOutCallback = () => this.shell.musicController.addSfx('shop-curtainclose')
-        new MoveTo(clothesroom)
+        // shopdoor (components)
+        const shopdoorButton = new Button(shopdoor)
+        shopdoorButton.hoverCallback = () => this.shell.musicController.addSfx('shop-dooropen')
+        shopdoorButton.hoverOutCallback = () => this.shell.musicController.addSfx('shop-doorclose')
+        const shopdoorMoveTo = new MoveTo(shopdoor)
+        shopdoorMoveTo.x = 1125
+        shopdoorMoveTo.y = 325
 
-        // spinner (components)
-        const spinnerSimpleButton = new SimpleButton(spinner)
-        spinnerSimpleButton.hoverCallback = () => {
-            this.onSpinnerOver()
-            this.shell.musicController.addSfx('shop-spinner')
-        }
-        spinnerSimpleButton.hoverOutCallback = () => {
-            this.onSpinnerOut()
-            this.shell.musicController.stopLoopingSfx('shop-spinner')
-        }
+        // cash (components)
+        const cashSimpleButton = new SimpleButton(cash)
+        cashSimpleButton.hoverCallback = () => this.onCashHover()
+        cashSimpleButton.hoverOutCallback = () => this.onCashOut()
 
-        // door (components)
-        const doorButton = new Button(door)
-        doorButton.hoverCallback = () => this.shell.musicController.addSfx('shop-dooropen')
-        doorButton.hoverOutCallback = () => this.shell.musicController.addSfx('shop-doorclose')
-        const doorMoveTo = new MoveTo(door)
-        doorMoveTo.x = 1100
-        doorMoveTo.y = 350
+        // chest (components)
+        new Button(chest)
 
-        // catalog_en (components)
-        const catalog_enButton = new Button(catalog_en)
-        catalog_enButton.hoverCallback = () => this.shell.musicController.addSfx('book-bookopen')
-        catalog_enButton.hoverOutCallback = () => this.shell.musicController.addSfx('book-bookclose')
-        catalog_enButton.callback = () => this.showClothingCat()
-        catalog_enButton.pixelPerfect = true
-        catalog_enButton.isLocalised = true
+        // catalog_old (components)
+        const catalog_oldButton = new Button(catalog_old)
+        catalog_oldButton.hoverCallback = () => this.shell.musicController.addSfx('book-bookopen')
+        catalog_oldButton.hoverOutCallback = () => this.shell.musicController.addSfx('book-bookclose')
+        catalog_oldButton.callback = () => this.showClothingCat()
+        catalog_oldButton.isLocalised = true
 
-        this.bg = bg
-        this.rail = rail
-        this.speakers = speakers
-        this.chairBody = chairBody
-        this.chairArm = chairArm
-        this.chairBody_1 = chairBody_1
-        this.chairArm_1 = chairArm_1
-        this.deskChair = deskChair
-        this.desk = desk
+        // catalog (components)
+        const catalogButton = new Button(catalog)
+        catalogButton.hoverCallback = () => this.onCatalogHover()
+        catalogButton.hoverOutCallback = () => this.onCatalogOut()
+        catalogButton.callback = () => this.showClothingCat()
+        catalogButton.isLocalised = true
+
+        this.cash = cash
+        this.train = train
         this.chest = chest
-        this.register = register
-        this.clothesroom = clothesroom
-        this.spinner = spinner
-        this.door = door
-        this.circularStool = circularStool
-        this.squareStool = squareStool
-        this.fg = fg
-        this.catalog_en = catalog_en
+        this.catalog_old = catalog_old
         this.unlock = unlock
+        this.catalog = catalog
         this.sort = sort
 
         this.events.emit('scene-awake')
@@ -199,28 +189,32 @@ export default class Shop extends RoomScene {
 
     create() {
         super.create()
-        this.speakers.play('shop-clothesspeakers')
-        this.spinner.on('animationcomplete', () => this.spinner.setFrame('spinner0001'))
+        this.train.play('shop-train')
     }
 
-    onSpinnerOver() {
-        if (this.spinner.anims.isPlaying) return
-        this.spinner.play('shop-spinner')
+    onCashHover() {
+        this.cash.play('shop-registerHover')
+        this.shell.musicController.addSfx('coffee-cashregister')
     }
 
-    onSpinnerOut() {
-        this.spinner.once('animationcomplete', this.stopSpinner, this)
-    }
-
-    stopSpinner() {
-        this.spinner.stop()
-        this.spinner.setFrame('spinner0001')
+    onCashOut() {
+        this.cash.stop()
+        this.cash.setFrame('cashJump0001')
     }
 
     showClothingCat() {
         this.interface.loadExternal('ClothingCatalog')
     }
 
+    onCatalogHover() {
+        this.catalog.setFrame('catalog-hover')
+        this.shell.musicController.addSfx('book-bookopen')
+    }
+
+    onCatalogOut() {
+        this.catalog.setFrame('catalog')
+        this.shell.musicController.addSfx('book-bookclose')
+    }
     /* END-USER-CODE */
 }
 
